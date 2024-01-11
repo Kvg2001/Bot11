c4e15e0b329abc915e493974d5062110e91697bd
Kvg2001 <154742660+Kvg2001@users.noreply.github.com>
2024-01-11 20:53:32 +0200

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,34 +2,20 @@ import discord
 from discord.ext import commands
 import mysql.connector
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-# Conectare la baza de date MySQL
 config = {
-    'host': '93.115.101.23:3306',
+    'host': '93.115.101.23',
     'user': 'u521_FSfdz1guUF',
     'password': 'jg9sfLwntt@StfB+tNG^7QRR',
     'database': 's521_webProject',
 }
 
-conn = mysql.connector.connect(**config)
-cursor = conn.cursor()
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
 
-# Comanda de creare a tabelului
-create_table_query = """
-CREATE TABLE IF NOT EXISTS testbot (
-    id BIGINT PRIMARY KEY,
-    nume VARCHAR(255),
-    discriminator VARCHAR(4)
-);
-"""
-
-cursor.execute(create_table_query)
-conn.commit()
+cursor.execute("CREATE TABLE IF NOT EXISTS testbot (id BIGINT, nume VARCHAR(50), puncte INT)")
+cnx.commit()
 
 @bot.event
 async def on_ready():
@@ -38,9 +24,42 @@ async def on_ready():
 @bot.command()
 async def register(ctx):
     user = ctx.author
-    cursor.execute('INSERT INTO testbot (id, nume, discriminator) VALUES (%s, %s, %s)',
-                   (user.id, user.name, user.discriminator))
-    conn.commit()
-    await ctx.send(f'{user.name} a fost Ã®nregistrat cu succes!')
+
+    
+    cursor.execute('SELECT nume FROM testbot WHERE id = %s', (user.name,))
+    rows = cursor.fetchall()
+    for nume in rows:
+           If ctx.author.name == name
+        await ctx.send(f'{user.name}, esti deja inregistrat! Nu este necesar sa te inregistrezi din nou.')
+  Else:
+    cursor.execute('INSERT INTO testbot (id, nume, puncte) VALUES (%s, %s, 0)', (user.id, user.name))
+    cnx.commit()
+    await ctx.send(f'{user.name} a fost inregistrat cu succes!')
+
+@bot.command()
+async def points(ctx):
+    user = ctx.author
+    cursor.execute("SELECT puncte FROM testbot WHERE nume = %s", (user.name,))
+    rows = cursor.fetchall()
+
+    for puncte in rows:
+        await ctx.send(f'Jucatorul *{user.name}* are ```{puncte[0]}``` puncte!')
+
+@bot.command()
+async def give(ctx, user: discord.Member, points: int):
+    cursor.execute("SELECT puncte FROM testbot WHERE nume = %s", ( user.name,))
+    row = cursor.fetchone()
+    for puncte in row:
+        punctenoi = puncte + points
+
+        cursor.execute("UPDATE testbot SET puncte = %s WHERE nume = %s", (punctenoi, user.name))
+        cnx.commit()
+        await ctx.send(f'Punctele pentru {user.mention} au fost actualizate la `{punctenoi}`!')
+
+
+@bot.event
+async def on_bot_close():
+    cursor.close()
+    cnx.close()
 
 bot.run('MTE5NDczOTE4MjU2ODk5NjkwNA.G2iUFc._YshhVl_MMSu_gn3W3TlqowfpVBzaDIN-XG2wU')
